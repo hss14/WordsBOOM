4cdc133afda2df0b4eed8bf698e224ba401f309d
ZhuJianjiang <zjjott@gmail.com>
2017-01-04 23:41:39 -0500

    first backup

---
+++ GUIversion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import Tkinter
+
+mainwin = Tkinter.Tk()
+mainwin.title("Let your vocabulary BOOMING!")
+mainwin.geometry('400x200')
+
+Tkinter.mainloop()
---
+++ table_setup.sql
@@ -0,0 +1,9 @@
+create table words (
+	word varbinary(25) not null primary key,
+	meaning char(40),
+	etyma char(20),
+        association char(30),
+	yes tinyint unsigned default 0,
+	no tinyint unsigned default 0,
+	ts timestamp default CURRENT_TIMESTAMP
+) character set utf8 collate utf8_general_ci;
